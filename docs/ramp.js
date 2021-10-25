a6843b245c8257a49bfc09ea49cbeaf0b2d51d0b
JPAbela <johnpauljosephabela@gmail.com>
2021-10-25 15:11:49 -0400

    ditto

--- docs/ramp.js
+++ docs/ramp.js
@@ -3,11 +3,10 @@ function setup()
   let canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
   canvas.position(windowWidth - width)/2, (windowHeight - height)/2);
   noStroke();
-  background(0);
 }
 
 function draw()
 {
-  // let back = color(0, 165, 255);
-  // background(back);
+  let back = color(0, 165, 255);
+  background(back);
 }
