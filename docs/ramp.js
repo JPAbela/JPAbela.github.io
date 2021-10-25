931cee8e69475da9062ed56aad90fc3e0c952b45
JPAbela <johnpauljosephabela@gmail.com>
2021-10-25 15:11:28 -0400

    try to add blue background but it doesn't work

--- docs/ramp.js
+++ docs/ramp.js
@@ -3,4 +3,11 @@ function setup()
   let canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
   canvas.position(windowWidth - width)/2, (windowHeight - height)/2);
   noStroke();
+  background(0);
+}
+
+function draw()
+{
+  // let back = color(0, 165, 255);
+  // background(back);
 }
