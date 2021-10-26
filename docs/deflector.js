3ff06386df8c5a7bca91dd0c39075802262d5b60
JPAbela <johnpauljosephabela@gmail.com>
2021-10-26 14:58:33 -0400

    deflector.html now gets script from deflector.js, make deflector

--- docs/deflector.js
+++ docs/deflector.js
@@ -2,11 +2,14 @@ function setup()
 {
   let canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
   canvas.position((windowWidth - width)/2, (windowHeight - height)/2);
-  noStroke();
+  strokeWeight(5);
+  stroke(150);
 }
 
 function draw()
 {
   let back = color(0, 165, 255);
   background(back);
+
+  line(width/10, height - 10, width/4, height - 10);
 }
