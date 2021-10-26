bac5c5d5c11a6597cd0bed228c21acace3f97706
JPAbela <johnpauljosephabela@gmail.com>
2021-10-26 14:45:39 -0400

    make blue background (turns out i had just been missing a parenthesis

--- docs/ramp.js
+++ docs/ramp.js
@@ -1,7 +1,7 @@
 function setup()
 {
   let canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
-  canvas.position(windowWidth - width)/2, (windowHeight - height)/2);
+  canvas.position((windowWidth - width)/2, (windowHeight - height)/2);
   noStroke();
 }
 
