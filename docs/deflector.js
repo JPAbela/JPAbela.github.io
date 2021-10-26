08942a7f5ebf62efcb9d64ef8a463e22bb2761ad
JPAbela <johnpauljosephabela@gmail.com>
2021-10-26 15:02:19 -0400

    position and size ramp better

--- docs/deflector.js
+++ docs/deflector.js
@@ -2,7 +2,7 @@ function setup()
 {
   let canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
   canvas.position((windowWidth - width)/2, (windowHeight - height)/2);
-  strokeWeight(5);
+  strokeWeight(20);
   stroke(150);
 }
 
@@ -11,5 +11,5 @@ function draw()
   let back = color(0, 165, 255);
   background(back);
 
-  line(width/10, height - 10, width/4, height - 10);
+  line(width/9, height - 40, width/3, height - 40);
 }
